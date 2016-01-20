67b10417521adb0b237d5b469a9ac9a154110d68
Nahuel <nahuel.palumbo@parsimotion.com>
2016-01-20 20:00:07 -0300

    Procesamiento continuo
    Todo el tiempo se va estar intentando procesar mensajes de la cola.
    La variable de entorno PARALLEL_PROCESS_COUNT indica cuántos jobs en
    paralelos se desea que estén procesando mensajes. DEFAULT = 1

--- App_Data/jobs/continuous/MassiveOperations/run.js
+++ App_Data/jobs/continuous/MassiveOperations/run.js
@@ -1,6 +1,25 @@
 require("coffee-script/register");
+var async = require('async');
 var job = require('massive-operations-webjob');
 
-job
-.create(process.env.STORAGE_NAME, process.env.STORAGE_KEY, process.env.BASE_URL_API)
-.processMessage(process.env.QUEUE_NAME);
+var processor = job.create(process.env.STORAGE_NAME, process.env.STORAGE_KEY, process.env.BASE_URL_API)
+
+var q = async.queue(function (queue, callback) {
+  processor.processMessage(queue)
+  .then(function() {
+  	console.log("PROCESS FINISHED SUCCESS");
+	callback()
+  })
+  .catch(function(err) {
+  	console.log("PROCESS FINISHED ERROR");
+  	console.log(err);
+    callback(err)
+  })
+}, process.env.PARALLEL_PROCESS_COUNT || 1);
+
+async.forever(
+  function(next) {
+  	q.push(process.env.QUEUE_NAME)
+  	next()
+  }
+);
