f6c7fb117021a828bd457cf062d9ce62d981f829
andreskir <andres@parsimotion.com>
2016-02-09 07:52:22 -0300

    jobsqueue

--- App_Data/jobs/continuous/requests/run.js
+++ App_Data/jobs/continuous/requests/run.js
@@ -8,7 +8,8 @@ options = {
   numOfMessages: process.env.REQUESTS_BATCH_SIZE || 16,
   visibilityTimeout: process.env.REQUESTS_VISIBILITY_TIMEOUT || 45,
   maxDequeueCount: process.env.REQUESTS_MAX_DEQUEUE_COUNT || 3,
-  concurrency: process.env.REQUESTS_CONCURRENCY || 48
+  concurrency: process.env.REQUESTS_CONCURRENCY || 48,
+  jobsQueue: false
 };
 
 job.run(options);
