c141b31be8be4e20c579e88e8ff3a9935815754e
andreskir <andres@parsimotion.com>
2016-02-29 09:46:37 -0300

    maxMessages

--- App_Data/jobs/continuous/jobs/run.js
+++ App_Data/jobs/continuous/jobs/run.js
@@ -5,7 +5,7 @@ options = {
   storageKey: process.env.STORAGE_KEY,
   baseUrl: process.env.BASE_URL_API,
   queue: process.env.JOBS_QUEUE,
-  numOfMessages: process.env.JOBS_BATCH_SIZE || 16,
+  maxMessages: process.env.JOBS_BATCH_SIZE || 16,
   visibilityTimeout: process.env.JOBS_VISIBILITY_TIMEOUT || 45,
   maxDequeueCount: process.env.JOBS_MAX_DEQUEUE_COUNT || 3,
   concurrency: process.env.JOBS_CONCURRENCY || 48,
--- App_Data/jobs/continuous/requests/run.js
+++ App_Data/jobs/continuous/requests/run.js
@@ -5,7 +5,7 @@ options = {
   storageKey: process.env.STORAGE_KEY,
   baseUrl: process.env.BASE_URL_API,
   queue: process.env.REQUESTS_QUEUE,
-  numOfMessages: process.env.REQUESTS_BATCH_SIZE || 16,
+  maxMessages: process.env.REQUESTS_BATCH_SIZE || 16,
   visibilityTimeout: process.env.REQUESTS_VISIBILITY_TIMEOUT || 45,
   maxDequeueCount: process.env.REQUESTS_MAX_DEQUEUE_COUNT || 3,
   concurrency: process.env.REQUESTS_CONCURRENCY || 48,
