d2aec490095eaf14b1151b61748301d058aff1d7
andreskir <andres@parsimotion.com>
2016-02-29 11:34:13 -0300

    chau visibilityTimeout

--- App_Data/jobs/continuous/requests/run.js
+++ App_Data/jobs/continuous/requests/run.js
@@ -6,7 +6,6 @@ options = {
   baseUrl: process.env.BASE_URL_API,
   queue: process.env.REQUESTS_QUEUE,
   maxMessages: process.env.REQUESTS_BATCH_SIZE || 16,
-  visibilityTimeout: process.env.REQUESTS_VISIBILITY_TIMEOUT || 45,
   maxDequeueCount: process.env.REQUESTS_MAX_DEQUEUE_COUNT || 3,
   concurrency: process.env.REQUESTS_CONCURRENCY || 48,
   jobsQueue: false
